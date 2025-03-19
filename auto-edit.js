73a81a7f4c061f169100886a12baaaffd8a65801
Alaric Holloway <alaric.holloway@gmail.com>
2025-03-18 23:47:13 -0400

    Implement auto-edit.js daemon mode

--- auto-edit.js
+++ auto-edit.js
@@ -1,18 +1,29 @@
-// node auto-edit.py src_alf dest_alf
+// node auto-edit.py [options] [src_alf dest_alf]
+//
+// Apply auto-edit annotations. Can be run as a command or a daemon. If run as a
+// command, annotations in src_alf are applied and written to dest_alf.
+// Otherwise processing is performed via Unix domain sockets.
+//
+// The following options are supported:
+//
+// --simplify
+//       Simplify algebraic expressions when applying auto-edit annotations.
+// --daemon
+//       Run as a daemon.
 
-const tyl = require('node-ty-levels');
 const process = require('process');
 const fs = require('fs');
+const net = require('net');
+const { TransformEdit } = require('node-ty-levels');
 
-if (process.argv.length < 4) {
-    console.error('Usage: node auto-edit.js src_file.alf out_file.alf')
-    process.exit(1);
-}
+const DAEMON_MODE = process.argv.includes('--daemon');
+const DO_SIMPLIFY = process.argv.includes('--simplify');
 
-const inFile = process.argv[2];
-const outFile = process.argv[3];
+const transformEdit = new TransformEdit({
+    simplifyExpressions: DO_SIMPLIFY
+});
 
-class TreeNode {
+class EditTreeNode {
     constructor(edits, parent) {
         this.edits = edits;
         this.parent = parent;
@@ -20,57 +31,70 @@ class TreeNode {
     }
 }
 
-const RGX_AUTO_BEGIN = /<!--\s*auto:\s*(.+?)\s*-->/;
-const RGX_AUTO_END = /<!--\s*\/auto\s*-->/;
+class EditTreeBuilder {
+    static RGX_AUTO_BEGIN = /<!--\s*auto:\s*(.+?)\s*-->/;
+    static RGX_AUTO_END = /<!--\s*\/auto\s*-->/;
 
-const splitEdits = editListString => {
-    const s = editListString + ',';
-    const edits = [];
-    let parens = 0;
-    for (let i = 0, tokenStart = 0; i < s.length; i++) {
-        if (s[i] === '(') {
-            parens++;
-            continue;
-        } else if (s[i] === ')') {
-            parens--;
-            continue;
-        } else if (s[i] === ',' && parens === 0) {
-            edits.push(s.substring(tokenStart, i));
-            tokenStart = i + 1;
+    static splitEdits(editListString) {
+        const s = editListString + ',';
+        const edits = [];
+        let parens = 0;
+        for (let i = 0, tokenStart = 0; i < s.length; i++) {
+            if (s[i] === '(') {
+                parens++;
+                continue;
+            } else if (s[i] === ')') {
+                parens--;
+                continue;
+            } else if (s[i] === ',' && parens === 0) {
+                edits.push(s.substring(tokenStart, i));
+                tokenStart = i + 1;
+            }
         }
+        if (parens !== 0) {
+            throw new Error(`Encountered malformed list of auto-edit commands "${editListString}".`);
+        }
+        return edits.map(edit => edit.trim());
     }
-    if (parens !== 0) {
-        throw new Error(`Encountered malformed list of auto-edit commands "${editListString}".`);
-    }
-    return edits.map(edit => edit.trim());
-};
 
-const buildTreeFromInFile = async () => {
-    const root = new TreeNode();
-    let curNode = root;
-    const f = await fs.promises.open(inFile);
-    for await (const line of f.readLines()) {
-        const results = line.match(RGX_AUTO_BEGIN);
+    constructor() {
+        this.root = new EditTreeNode();
+        this.curNode = this.root;
+    }
+
+    readLine(line) {
+        const results = line.match(EditTreeBuilder.RGX_AUTO_BEGIN);
         if (results) {
-            const edits = splitEdits(results[1]);
-            const newNode = new TreeNode(edits, curNode);
-            curNode.children.push(newNode);
-            curNode = newNode;
-            continue;
-        }
-        if (RGX_AUTO_END.test(line)) {
-            if (!curNode.parent) {
+            const edits = EditTreeBuilder.splitEdits(results[1]);
+            const newNode = new EditTreeNode(edits, this.curNode);
+            this.curNode.children.push(newNode);
+            this.curNode = newNode;
+            return;
+        } else if (EditTreeBuilder.RGX_AUTO_END.test(line)) {
+            if (!this.curNode.parent) {
                 throw new Error('Encountered auto-edit end tag without matching start tag.');
             }
-            curNode = curNode.parent;
-            continue;
+            this.curNode = this.curNode.parent;
+            return;
         }
-        curNode.children.push(line);
+        this.curNode.children.push(line);
     }
-    if (curNode !== root) {
-        throw new Error('Expected auto-edit end tag, but reached end of document.');
+
+    eof() {
+        if (this.curNode !== this.root) {
+            throw new Error('Expected auto-edit end tag, but reached end of document.');
+        }
+        return this.root;
     }
-    return root;
+}
+
+const buildEditTreeFromFile = async inFile => {
+    const builder = new EditTreeBuilder();
+    const f = await fs.promises.open(inFile);
+    for await (const line of f.readLines()) {
+        builder.readLine(line);
+    }
+    return builder.eof();
 };
 
 const aliases = {
@@ -84,9 +108,9 @@ const aliases = {
     rcc: 'rotate90Counterclockwise'
 };
 
-const renderTree = node => {
+const renderEditTree = node => {
     const chunks = [];
-    node.children.forEach(child => chunks.push(child instanceof TreeNode ? renderTree(child) : child));
+    node.children.forEach(child => chunks.push(child instanceof EditTreeNode ? renderEditTree(child) : child));
     let mergedChunk = chunks.join('\n');
     if (node.edits) {
         node.edits.forEach(edit => {
@@ -96,7 +120,7 @@ const renderTree = node => {
             }
             const fnName = results[1];
             const args = results[3] ? results[3].split(',') : [];
-            const editFn = tyl[fnName] || tyl[aliases[fnName]];
+            const editFn = transformEdit[fnName] || transformEdit[aliases[fnName]];
             if (!editFn) {
                 throw new Error(`Encountered unrecognized edit command "${fnName}".`);
             }
@@ -106,17 +130,96 @@ const renderTree = node => {
     return mergedChunk;
 };
 
-(async () => {
-    console.log(`auto-edit.js -> Processing ${inFile}...`);
-    try {
-        const root = await buildTreeFromInFile();
-        let renderedAlf = renderTree(root);
-        if (!process.env.NO_SIMPLIFY) {
-            renderedAlf = tyl.simplify(renderedAlf);
+const renderEditTreeAfterFinalSimplify = node => {
+    const alf = renderEditTree(node);
+    // Apply one final simplification pass to catch attributes unaffected by auto-edits.
+    return DO_SIMPLIFY ? transformEdit.simplify(alf) : alf;
+};
+
+class AutoEditServer {
+    static SOCKET_PATH = 'var/auto-edit.sock';
+    static EOF_STRING = '<<<EOF>>>';
+
+    start() {
+        if (this.server) {
+            return;
         }
-        fs.writeFileSync(outFile, renderedAlf);
-    } catch (e) {
-        console.error(`auto-edit.js -> Error: ${e.message}`);
+
+        if (fs.existsSync(AutoEditServer.SOCKET_PATH)) {
+            fs.unlinkSync(AutoEditServer.SOCKET_PATH);
+        }
+
+        this.server = net.createServer(client => {
+            console.log('auto-edit.js -> Processing incoming ALF stream...');
+            
+            let buffer = [];
+        
+            client.on('data', data => {
+                data = data.toString('utf-8');
+                let eof = false;
+                const eofIndex = data.indexOf(AutoEditServer.EOF_STRING);
+                if (eofIndex >= 0) {
+                    data = data.substring(0, eofIndex);
+                    eof = true;
+                }
+                buffer.push(data);
+                if (!eof) {
+                    return;
+                }
+                buffer = buffer.join('');
+                const builder = new EditTreeBuilder();
+                let lineStart = 0;
+                for (let i = 0; i < buffer.length; i++) {
+                    if (buffer[i] === '\n') {
+                        builder.readLine(buffer.substring(lineStart, i));
+                        lineStart = i + 1;
+                    }
+                }
+                if (lineStart < buffer.length) {
+                    builder.readLine(buffer.substring(lineStart, buffer.length));
+                }
+                client.write(renderEditTreeAfterFinalSimplify(builder.eof()) + AutoEditServer.EOF_STRING);
+            });
+        });
+
+        this.server.listen(AutoEditServer.SOCKET_PATH, () => {
+            console.log(`auto-edit.js -> Daemon listening on ${AutoEditServer.SOCKET_PATH}...`);
+        });
+    
+        process.on('SIGINT', () => {
+            console.log('auto-edit.js -> Received SIGINT. Shutting down...');
+            this.server.close(() => {
+                if (fs.existsSync(AutoEditServer.SOCKET_PATH)) {
+                    fs.unlinkSync(AutoEditServer.SOCKET_PATH);
+                }
+                process.exit(0);
+            });
+        });
+    }
+}
+
+if (DAEMON_MODE) {
+    fs.mkdirSync('var', { recursive: true });
+    const server = new AutoEditServer();
+    server.start();
+} else {
+    if (process.argv.length < 4) {
+        console.error('Usage: node auto-edit.js [options] src_file.alf out_file.alf')
         process.exit(1);
     }
-})();
+    
+    const inFile = process.argv[process.argv.length - 2];
+    const outFile = process.argv[process.argv.length - 1];
+
+    (async () => {
+        console.log(`auto-edit.js -> Processing ${inFile}...`);
+        try {
+            const root = await buildEditTreeFromFile(inFile);
+            let renderedAlf = renderEditTreeAfterFinalSimplify(root);
+            fs.writeFileSync(outFile, renderedAlf);
+        } catch (e) {
+            console.error(`auto-edit.js -> Error: ${e.message}`);
+            process.exit(1);
+        }
+    })();
+}
--- dist.py
+++ dist.py
@@ -12,8 +12,13 @@
 #       Strip etags.
 # --crush_floats
 #       Truncate floats to fourth decimal place.
+# --debug
+#       Set DEBUG global to true in ALF template environment
+# --use-auto-edit-daemon
+#       Use auto-edit daemon instead of running auto-edit.js directly. The
+#       daemon must be running already.
 
-import sys, os, re
+import sys, os, re, socket
 from jinja2 import Environment, FileSystemLoader, StrictUndefined
 from lxml import etree
 from alf_globals import alf_globals
@@ -24,12 +29,13 @@ DO_TRIM = '--trim' in args
 DO_STRIP_ETAGS = '--strip-etags' in args
 DO_CRUSH_FLOATS = '--crush-floats' in args
 DO_SIMPLIFY = '--simplify' in args
+USE_AUTO_EDIT_DAEMON = '--use-auto-edit-daemon' in args
 DEBUG = '--debug' in args
 
 fin = args[len(args) - 2]
 fout = args[len(args) - 1]
 
-print(f'Rendering {fout}...')
+print(f'dist.py -> Rendering {fout}...')
 
 class RelativeEnvironment(Environment):
     """Override join_path() to enable relative template paths."""
@@ -69,27 +75,52 @@ def getContextHighlighted(s, line, offset, n):
 def printError(msg):
     print(f'\033[1;35m{msg}\033[0m')
 
+def autoEditViaFiles(alf):
+    os.system('mkdir -p tmp')
+    alfFileName = os.path.basename(fin)
+    tmpPrePath = f'tmp/{alfFileName}'
+    tmpPostPath = f'tmp/{alfFileName}-auto-edit'
+    with open(tmpPrePath, 'w') as fh:
+        fh.write(alf)
+    
+    autoEditShellCommand = f'node auto-edit.js {' '.join(args[1:-2])} {tmpPrePath} {tmpPostPath}'
+    status = os.system(autoEditShellCommand)
+    if status != 0:
+        if status == 32512:
+            raise Exception(f'auto-edit.js exited with status {status}. Is your Node.js environment activated?')
+        raise Exception(f'auto-edit.js exited with status {status}.')
+    with open(tmpPostPath, 'r') as fh:
+        return fh.read()
+
+SOCKET_PATH = 'var/auto-edit.sock'
+EOF_STRING = '<<<EOF>>>'
+
+def autoEditViaSocket(alf: str):
+    try:
+        sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        sock.connect(SOCKET_PATH)
+        sock.sendall((alf + EOF_STRING).encode('utf-8'))
+        response = []
+        boundaryChars = ''
+        while True:
+            chunk = sock.recv(1024).decode()
+            if not chunk:
+                raise Exception('auto-edit daemon terminated connection prematurely.')
+            response.append(chunk)
+            # EOF string could fall on chunk boundary
+            if EOF_STRING in chunk or EOF_STRING in boundaryChars + chunk[0:len(EOF_STRING) - 1]:
+                break
+            boundaryChars = chunk[1 - len(EOF_STRING):]
+        response = ''.join(response)
+        return response[0 : len(response) - len(EOF_STRING)]
+    finally:
+        sock.close()
+
 try:
     alf = env.get_template(fin).render()
 
     if re.search(r'<!--\s*auto:\s*(.+?)\s*-->', alf):
-        os.system('mkdir -p tmp')
-        alfFileName = os.path.basename(fin)
-        tmpPrePath = f'tmp/{alfFileName}'
-        tmpPostPath = f'tmp/{alfFileName}-auto-edit'
-        with open(tmpPrePath, 'w') as fh:
-            fh.write(alf)
-        autoEditShellCommand = f'node auto-edit.js {tmpPrePath} {tmpPostPath}'
-        # [TODO] Consider using PyMiniRacer to avoid overhead of reloading JavaScript every time
-        opts = '' if DO_SIMPLIFY else 'NO_SIMPLIFY=true'
-        autoEditShellCommand = f'{opts} node auto-edit.js {tmpPrePath} {tmpPostPath}'
-        status = os.system(autoEditShellCommand)
-        if status != 0:
-            if status == 32512:
-                raise Exception(f'auto-edit.js exited with status {status}. Is your Node.js environment activated?')
-            raise Exception(f'auto-edit.js exited with status {status}.')
-        with open(tmpPostPath, 'r') as fh:
-            alf = fh.read()
+        alf = autoEditViaSocket(alf) if USE_AUTO_EDIT_DAEMON else autoEditViaFiles(alf)
 
     parser = etree.XMLParser(remove_blank_text=DO_TRIM, remove_comments=DO_TRIM)
     tree = etree.XML(alf, parser)
